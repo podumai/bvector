d020202f77602912734cd4a246c8bd8000fcd720
podumai <kirillsm05@gmail.com>
2024-10-04 19:11:56 +0700

    small changes in hpp f

--- SECOND_COURSE/OOP/bit-array/CMakeFiles/BIT_ARRAY_LIB.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libBIT_ARRAY_LIB.a"
-)
--- SECOND_COURSE/OOP/bit-array/source/main.cpp
+++ SECOND_COURSE/OOP/bit-array/source/main.cpp
@@ -1,23 +1,31 @@
 #include <cinttypes>
 #include <iostream>
+#include <chrono>
 #include <vector>
+#include <fstream>
 #include <iomanip>
-#include "bit_array.hpp"
+#include <cstddef>
+#include "bvector.hpp"
 
 std::int32_t main()
 {
   std::cout.setf(std::ios::boolalpha);
-  
-  bits::bit_array test1;
-  //test1.reserve(268'435'456);
 
-  for (std::int32_t i {}; i < INT32_MAX; ++i)
+  bits::bvector test1;
+  test1.resize(INT32_MAX, true);
+  for (std::size_t i {}; i < INT32_MAX; ++i)
   {
     test1.push_back(true);
   }
-
   std::cout << test1.count() << std::endl;
-  std::cout << "max_size: " << test1.max_size() << std::endl;
-  
+  /*try
+  {
+    std::cout << test.to_string() << std::endl;
+  }
+  catch (std::bad_alloc &err)
+  {
+    std::cerr << "  what(): " << err.what() << std::endl;
+  }*/
+
   return 0;
 }
